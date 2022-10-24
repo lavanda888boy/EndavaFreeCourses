399b2c0829b635b1d6b1745691dbe2dcd5765be6
Seva Bajenov <sevabaj@gmail.com>
2022-10-24 09:57:51 +0300

    Initialize first Spring Web project.

---
+++ src/main/java/com/webapp/endavacourseproject/EndavaCourseProjectApplication.java
@@ -0,0 +1,13 @@
+package com.webapp.endavacourseproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EndavaCourseProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EndavaCourseProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/webapp/endavacourseproject/EndavaCourseProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.webapp.endavacourseproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EndavaCourseProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
