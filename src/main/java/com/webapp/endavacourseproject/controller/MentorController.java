93306e2a44498590e3195dae2a41b01bc82d1d1b
Seva Bajenov <sevabaj@gmail.com>
2022-10-31 12:52:30 +0200

    Add the scratch of service and controller classes.

---
+++ src/main/java/com/webapp/endavacourseproject/controller/MentorController.java
@@ -0,0 +1,11 @@
+package com.webapp.endavacourseproject.controller;
+
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@AllArgsConstructor
+@RequestMapping("/mentors")
+public class MentorController {
+}
---
+++ src/main/java/com/webapp/endavacourseproject/service/IndustryService.java
@@ -0,0 +1,9 @@
+package com.webapp.endavacourseproject.service;
+
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@AllArgsConstructor
+public class IndustryService {
+}
