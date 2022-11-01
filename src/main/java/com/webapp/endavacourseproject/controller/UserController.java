7e7c0ecb6ae5c4fb8e05bd87400ad9d77f027679
Seva Bajenov <sevabaj@gmail.com>
2022-11-01 17:54:13 +0200

    Add basic rest methods to the UserController class.

--- src/main/java/com/webapp/endavacourseproject/controller/UserController.java
+++ src/main/java/com/webapp/endavacourseproject/controller/UserController.java
@@ -1,12 +1,55 @@
 package com.webapp.endavacourseproject.controller;
 
+import com.webapp.endavacourseproject.model.dto.UserDTO;
+import com.webapp.endavacourseproject.service.UserService;
 import lombok.AllArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @AllArgsConstructor
 @RequestMapping("/users")
 public class UserController {
 
+    private final UserService userService;
+
+    @PostMapping("/add")
+    public ResponseEntity<Object> addNewUser(@RequestBody UserDTO userDTO){
+        try {
+            userService.add(userDTO);
+            return new ResponseEntity<>(HttpStatus.CREATED);
+        } catch (Exception e) {
+            return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
+        }
+    }
+
+    @GetMapping("/")
+    public ResponseEntity<Object> getAllUsers(@RequestBody(required = false) Long limit){
+        try {
+             return new ResponseEntity<>(userService.getAll(limit), HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
+        }
+    }
+
+    @PatchMapping("update/{id}")
+    public ResponseEntity<Object> updateUser(@PathVariable Long id, @RequestBody UserDTO userDTO){
+        try {
+            userService.update(id, userDTO);
+            return new ResponseEntity<>(HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
+        }
+    }
+
+    @DeleteMapping("delete/{id}")
+    public ResponseEntity<Object> deleteUser(@PathVariable Long id, @RequestBody UserDTO userDTO){
+        try {
+            userService.delete(id, userDTO);
+            return new ResponseEntity<>(HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
+        }
+    }
 }
