b02a861a96eaadf4ddf6152d066f1792c9f8d65d
Seva Bajenov <sevabaj@gmail.com>
2022-11-03 19:48:10 +0200

    Add rest methods to the Mentor and Industry controllers.

--- src/main/java/com/webapp/endavacourseproject/controller/IndustryController.java
+++ src/main/java/com/webapp/endavacourseproject/controller/IndustryController.java
@@ -1,11 +1,55 @@
 package com.webapp.endavacourseproject.controller;
 
+import com.webapp.endavacourseproject.model.dto.IndustryDTO;
+import com.webapp.endavacourseproject.service.IndustryService;
 import lombok.AllArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @AllArgsConstructor
 @RequestMapping("/industries")
 public class IndustryController {
+
+    private final IndustryService industryService;
+
+    @PostMapping("/add")
+    public ResponseEntity<Object> addNewIndustry(@RequestBody IndustryDTO industryDTO){
+        try {
+            industryService.add(industryDTO);
+            return new ResponseEntity<>(HttpStatus.CREATED);
+        } catch (Exception e) {
+            return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
+        }
+    }
+
+    @GetMapping("/")
+    public ResponseEntity<Object> getAllIndustries(@RequestBody(required = false) Long limit){
+        try {
+            return new ResponseEntity<>(industryService.getAll(limit), HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
+        }
+    }
+
+    @PatchMapping("update/{id}")
+    public ResponseEntity<Object> updateIndustry(@PathVariable Long id, @RequestBody IndustryDTO industryDTO){
+        try {
+            industryService.update(id, industryDTO);
+            return new ResponseEntity<>(HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
+        }
+    }
+
+    @DeleteMapping("delete/{id}")
+    public ResponseEntity<Object> deleteIndustry(@PathVariable Long id){
+        try {
+            industryService.delete(id);
+            return new ResponseEntity<>(HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
+        }
+    }
 }
--- src/main/java/com/webapp/endavacourseproject/controller/MentorController.java
+++ src/main/java/com/webapp/endavacourseproject/controller/MentorController.java
@@ -1,11 +1,55 @@
 package com.webapp.endavacourseproject.controller;
 
+import com.webapp.endavacourseproject.model.dto.MentorDTO;
+import com.webapp.endavacourseproject.service.MentorService;
 import lombok.AllArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @AllArgsConstructor
 @RequestMapping("/mentors")
 public class MentorController {
+
+    private final MentorService mentorService;
+
+    @PostMapping("/add")
+    public ResponseEntity<Object> addNewMentor(@RequestBody MentorDTO mentorDTO){
+        try {
+            mentorService.add(mentorDTO);
+            return new ResponseEntity<>(HttpStatus.CREATED);
+        } catch (Exception e) {
+            return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
+        }
+    }
+
+    @GetMapping("/")
+    public ResponseEntity<Object> getAllMentors(@RequestBody(required = false) Long limit){
+        try {
+            return new ResponseEntity<>(mentorService.getAll(limit), HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
+        }
+    }
+
+    @PatchMapping("update/{id}")
+    public ResponseEntity<Object> updateMentor(@PathVariable Long id, @RequestBody MentorDTO mentorDTO){
+        try {
+            mentorService.update(id, mentorDTO);
+            return new ResponseEntity<>(HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
+        }
+    }
+
+    @DeleteMapping("delete/{id}")
+    public ResponseEntity<Object> deleteMentor(@PathVariable Long id){
+        try {
+            mentorService.delete(id);
+            return new ResponseEntity<>(HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
+        }
+    }
 }
