346b95ff38a899803061e4cab13c86a651d46532
Seva Bajenov <sevabaj@gmail.com>
2022-11-30 18:20:53 +0200

    Add @Valid annotation to the controllers.

--- src/main/java/com/webapp/endavacourseproject/controller/MentorController.java
+++ src/main/java/com/webapp/endavacourseproject/controller/MentorController.java
@@ -8,8 +8,11 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
+
 @RestController
 @AllArgsConstructor
+@Valid
 @RequestMapping("/mentors")
 public class MentorController {
 
